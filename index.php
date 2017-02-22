27f9e8ea28af93437968605238343c586c88a5b5
Jimmy <jimmyk3.14159@gmail.com>
2017-02-22 16:32:02 -0600

    Update index.php

--- index.php
+++ index.php
@@ -1,13 +1,26 @@
 <!doctype html>
 <html>
-  <head>
-    <title>Bomtush</title>
-      <style>
-			  nav {
-			  }
-		  </style>
-	  </head>
+	<head>
+		<title>Bomtush</title>
+		<style>
+			html {
+				margin: 0;
+			}
+			
+			header {
+				background-color: DarkSlateBlue;
+				height: 30px;
+			}
+			
+			header h1 {
+				color: White;
+				font: 20px bold sans-serif;
+			}
+		</style>
+	</head>
 	<body>
-		
+		<header>
+			<h1>bomtush</h1>
+		</header>
 	</body>
 </html>
