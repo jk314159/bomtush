985b7846ab2c8545f856249c104baf287ebbbd9e
Jimmy <jimmyk3.14159@gmail.com>
2017-03-01 16:14:54 -0600

    Create postpage.php

---
+++ postpage.php
@@ -0,0 +1,27 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>bootstrap will be the death of me</title>
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+    <link href="css/style.css" rel="stylesheet">
+    <link href="css/octicons.css" rel="stylesheet">
+  </head>
+  <body>
+    <div class="container-fluid">
+      <form action="post.php" method="post">
+        <div class="card">
+          <div class="card-block">
+            <textarea columns="20"></textarea>
+          </div>
+        </div>
+      </form>
+    </div>
+
+    <script src="js/jquery.min.js"></script>
+    <script src="js/tether.min.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+  </body>
+</html>
